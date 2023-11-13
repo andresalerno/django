2f0f2310df691fe0ab835060dfe07ac98521490f
andresalerno <andresalerno22@gmail.com>
2023-11-13 11:02:07 -0300

    new commit

---
+++ orm/Django-4.x-ORM-Course/codebase-0/inventory/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Brand(models.Model):
+    brand_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    the_name = models.CharField("Product Name", max_length=100, default="no-name", help_text="This is the help text")
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ["age"]
+    
+    def __str__(self):
+        return f"Product name: {self.the_name}"
+
+class Stock(models.Model):
+    units = models.BigIntegerField()
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.product)
