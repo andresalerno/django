96591e7fd5b98a58c9a328500e08ab0e93d9aa0e
andresalerno <andresalerno22@gmail.com>
2024-01-20 11:04:21 -0300

    chapter - 36 react routing introduction

--- full_stack/reactchat/src/App.tsx
+++ full_stack/reactchat/src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <></>;
-}
+// Import the 'Home' component from the "./pages" directory
+import Home from "./pages/Home";
 
+// Import necessary components and functions from the 'react-router-dom' library
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// Create a router using 'createBrowserRouter' and define routes using 'createRoutesFromElements'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // Define a single route for the path "/" that renders the 'Home' component
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+// Define the main 'App' component
+const App = () => {
+  // Return the 'RouterProvider' component, providing the 'router' object as a prop
+  return <RouterProvider router={router} />;
+};
+
+// Export the 'App' component as the default export of this module
 export default App;
